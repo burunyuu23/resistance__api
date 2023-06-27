87a9c1b6a873ab3dc649e9e37559db74bd8fad4f
danilkk <ezzfvkoko@gmail.com>
2023-06-27 12:35:34 +0300

    add exception's construct with HttpStatus and msg

--- resistanceAPI/src/main/java/com/dnlkk/resistance/exceptions/AppException.java
+++ resistanceAPI/src/main/java/com/dnlkk/resistance/exceptions/AppException.java
@@ -1,4 +1,16 @@
 package com.dnlkk.resistance.exceptions;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.http.HttpStatus;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 public class AppException extends RuntimeException{
+    private final HttpStatus status;
+
+    public AppException(String message, HttpStatus status) {
+        super(message);
+        this.status = status;
+    }
 }
--- resistanceAPI/src/main/java/com/dnlkk/resistance/exceptions/VertexNotFoundException.java
+++ resistanceAPI/src/main/java/com/dnlkk/resistance/exceptions/VertexNotFoundException.java
@@ -1,4 +1,9 @@
 package com.dnlkk.resistance.exceptions;
 
+import org.springframework.http.HttpStatus;
+
 public class VertexNotFoundException extends AppException{
+    public VertexNotFoundException() {
+        super("Vertex not found", HttpStatus.NOT_FOUND);
+    }
 }

9aeb39cd6a25a19f3ad68a65c1a77ea4e07bde1a
danilkk <ezzfvkoko@gmail.com>
2023-07-02 22:02:47 +0300

    fix AppException

--- src/main/java/com/dnlkk/resistance/dto/responses/ErrorResponseDTO.java
+++ src/main/java/com/dnlkk/resistance/dto/responses/ErrorResponseDTO.java
@@ -1,5 +1,7 @@
 package com.dnlkk.resistance.dto.responses;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
@@ -16,4 +18,9 @@ public class ErrorResponseDTO {
             description = "Error status",
             example = "404 NOT_FOUND")
     private HttpStatus status;
+
+    public ErrorResponseDTO(String message, HttpStatus status) {
+        this.message = message;
+        this.status = status;
+    }
 }
--- src/main/java/com/dnlkk/resistance/exceptions/AppException.java
+++ src/main/java/com/dnlkk/resistance/exceptions/AppException.java
@@ -1,5 +1,7 @@
 package com.dnlkk.resistance.exceptions;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.http.HttpStatus;
@@ -7,10 +9,11 @@ import org.springframework.http.HttpStatus;
 @EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 @Data
 public class AppException extends RuntimeException{
-    @EqualsAndHashCode.Include
     private final HttpStatus status;
 
-    public AppException(String message, HttpStatus status) {
+    @JsonCreator
+    public AppException(@JsonProperty("message") String message,
+                        @JsonProperty("status") HttpStatus status) {
         super(message);
         this.status = status;
     }
